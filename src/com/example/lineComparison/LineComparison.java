e71ceeda7eb2b01d16cece3b6d774a23b598f525
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-28 14:38:14 +0530

    Added -Length of line

--- src/com/example/lineComparison/LineComparison.java
+++ src/com/example/lineComparison/LineComparison.java
@@ -1,7 +1,27 @@
 package com.example.lineComparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to line comparison");
+
+        Scanner sc = new Scanner(System.in);
+        //Taking values of both points from user
+        System.out.println("Enter x-cordinate of first point:");
+        double x1 = sc.nextDouble();
+        System.out.println("Enter y-cordinate of first point:");
+        double y1 = sc.nextDouble();
+        System.out.println("Enter x-cordinate of second point:");
+        double x2 = sc.nextDouble();
+        System.out.println("Enter y-cordinate of second point:");
+        double y2 = sc.nextDouble();
+        sc.close();
+
+        //Calulating length of line
+        double lenghtOfline = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2- y1), 2));
+        System.out.println("Lenght of line : "+ lenghtOfline);
+
+
+
     }
 }
